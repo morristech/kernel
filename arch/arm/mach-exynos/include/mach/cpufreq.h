2ebdb88395629081d8e4ede83fcd5daabe4c0d5f
Gustavo <gustavo.santos0106@gmail.com>
2013-06-30 16:08:17 -0300

    mali: add clock & voltage control

--- arch/arm/mach-exynos/include/mach/cpufreq.h
+++ arch/arm/mach-exynos/include/mach/cpufreq.h
@@ -18,7 +18,8 @@ enum cpufreq_level_index {
 	L0, L1, L2, L3, L4,
 	L5, L6, L7, L8, L9,
 	L10, L11, L12, L13, L14,
-	L15, L16,
+	L15, L16, L17, L18, L19,
+	L20,
 };
 
 enum busfreq_level_request {
@@ -106,6 +107,8 @@ struct exynos_dvfs_info {
 	unsigned int	max_support_idx;
 	unsigned int	min_support_idx;
 	unsigned int	gov_support_freq;
+        unsigned int    max_current_idx;
+        unsigned int    min_current_idx;
 	struct clk	*cpu_clk;
 	unsigned int	*volt_table;
 	struct cpufreq_frequency_table	*freq_table;
